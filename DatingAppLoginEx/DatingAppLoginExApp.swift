b105255b28e3c89cb281925cb0f13f42867e0479
EserLodhia <eserlodhia@gmail.com>
2023-02-02 15:16:23 +0530

    Initial Commit

---
+++ DatingAppLoginEx/DatingAppLoginExApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingAppLoginExApp.swift
+//  DatingAppLoginEx
+//
+//  Created by Eser Lodhia on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DatingAppLoginExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
